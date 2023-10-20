727c2949b16211359e890c69dcebe73809363db6
Eduarda0508 <maria.silva87@estudante.ifms.edu.br>
2023-10-20 11:01:16 -0400

    aula 20/10

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,30 +1,38 @@
 import './Contato.css'
+import { useState } from 'react';
+
 function Contato(){
+    const [contatoTexto, setContatoTexto]=useState("")
+    function TrataContatoTexto(e:React.ChangeEvent<HTMLInputElement>){
+        setContatoTexto(e.target.value)
+    }
+
     return(
+        <div className='contatosM'>
         <form>
-            <fieldset>
-                <div>
-                    <h1>Envie uma mensagem!</h1>
-                    <label>Nome:</label>
-                    <input type="text" name="nome" id="nome"/>
-                </div>
-                <div>
-                <label>E-mail:</label>
-                <input type="text" name="e-mail" id="e-mail" />
+        <div className="dados">
+            <h1>Envie uma mensagem para mim!</h1>
+
+          
+            <label>Nome:</label>
+            <input className='input' type="text" placeholder='Nome' onChange={TrataContatoTexto}/>
+         
+            <label>E-mail:</label>
+            <input className='input' type='email' placeholder='E-mail' onChange={TrataContatoTexto}/>
+
+            <label>Telefone:</label>
+            <input className='input' type='fone' placeholder='Telefone' onChange={TrataContatoTexto}/>
+            </div>
+            
+          <button>Enviar</button>
+        
+            <div className="contatoTexto">
+             {contatoTexto && <p>{contatoTexto}</p>}
             </div>
 
-            <div>
-                <label>Telefone:</label>
-                <input type="text" name="telefone" id="telefone" />
-            </div>
-            <div>
-                <label>Mensagem</label>
-                <textarea placeholder=''></textarea>
-            </div>
-            </fieldset>
-            
-        </form>
+            </form>
+      </div>
     )
-   
 }
+
 export default Contato
